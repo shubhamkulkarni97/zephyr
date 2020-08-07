73a8015bedb08d250990eb8f63f551892c7882d0
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2020-08-07 18:06:40 +0530

    Code cleanup

--- drivers/wifi/esp32/include/stubs.h
+++ drivers/wifi/esp32/include/stubs.h
@@ -3,12 +3,4 @@
 
 uint32_t phy_enter_critical(void);
 void phy_exit_critical(uint32_t level);
-
-size_t strnlen(const char *s, size_t maxlen);
-
 void abort(void);
-void roundup2(void);
-void ets_delay_us(void);
-void gpio_output_set(void);
-void gpio_output_set_high(void);
-unsigned int crc32_le(unsigned int crc, unsigned char const * buf,unsigned int len);
